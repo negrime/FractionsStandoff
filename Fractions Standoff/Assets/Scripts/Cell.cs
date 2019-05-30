63daae0ee1535672f00c3b380ae9c71aee56ff0d
negrimez <scorpion36rus@gmail.com>
2019-05-30 21:07:17 +0300

    CellsGenrator

---
+++ Fractions Standoff/Assets/Scripts/Cell.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public bool isReserved;
+    private bool _isMouse;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1) && _isMouse)
+        {
+            isReserved = false;
+            
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!isReserved)
+        GetComponent<SpriteRenderer>().color = Color.green;
+
+        _isMouse = true;
+
+    }
+
+    private void OnMouseExit()
+    {
+        if (!isReserved)
+        GetComponent<SpriteRenderer>().color = Color.black;
+        _isMouse = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(1))
+            isReserved = false;
+        isReserved = true;
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseOver()
+    {
+        if (!isReserved)
+            GetComponent<SpriteRenderer>().color = Color.green;
+    }
+}
+    
---
+++ Fractions Standoff/Assets/Scripts/CellGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellGenerator : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform parent;
+    private Vector2 _position;
+    public GameObject cell; 
+    
+    void Start()
+    {
+        _position = startPosition.position;
+        for (int i = 0; i < 16; i++)
+        {
+            for (int j = 0; j < 16; j++)
+            {
+                var go = Instantiate(cell,  new Vector3(_position.x, _position.y, -0.1f), Quaternion.identity, parent);
+                _position = new Vector2(_position.x + 1, _position.y);
+            }
+            _position = new Vector2(_position.x - 16, _position.y - 1);
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
